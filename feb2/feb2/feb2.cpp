622a44c24b132bcf77a6b383047adadd24f0565a
game110pm <game110pm@605-110-STU19>
2017-02-06 13:15:02 -0700

    DAILY CODE

---
+++ feb2/feb2/feb2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int input;
+
+	cout<< "what song do you want played?" << endl;
+	cin >> input;
+	if (input == 1)
+	cout << "you never said goodbye" << endl;
+	else if (input == 2)
+	cout << "hey lucy I remember your name" << endl;
+	else if (input == 3)
+	cout << "you betrayed me all this tiiimeee" << endl;
+}
