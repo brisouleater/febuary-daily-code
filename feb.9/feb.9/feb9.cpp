c88a88a0849016bf3c9865547ed64dab1ce803e0
game110pm <game110pm@605-110-STU19>
2017-02-09 12:36:56 -0700

    feb9

---
+++ feb.9/feb.9/feb9.cpp
@@ -0,0 +1,6 @@
+# include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i < 200; i++)
+		cout << "So now I ask you, how much blood will you give in order to survive"<< endl;
+}
---
+++ feb7th/feb7th/feb7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int input;
+
+	cout << "how old are you?" << endl;
+	cin >> input;
+
+	if(input== even)
+		cout << "is your name steven?" << endl;
+	
+	else if (input == odd)
+		cout << "your name is odd" << endl;
+}
